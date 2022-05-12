786c019e07be0b70ddd4542264adc6ec81608c71
BENSALAH Seifeddine <seifeddine.bensalah@esprit.tn>
2022-05-12 11:16:46 +0100

    login fix

--- back/controller/UserController.php
+++ back/controller/UserController.php
@@ -128,30 +128,27 @@ class UserContoller
         $db = config::getConnexion();
         try {
 
-            +$query = $db->prepare($sql);
+            $query = $db->prepare($sql);
             $query->execute();
             $user = $query->fetch();
             // var_dump($user['email']);
-            if ($user != null) {
 
-                $email = $user['email'];
-                $password = $user['password'];
-                $role = $user['role'];
-                $id = $user['id'];
-                $count = $query->rowCount();
-                if ($count == 1) {
 
-                    $_SESSION['username'] = $username;
-                    $_SESSION['password'] = $password;
-                    $_SESSION['role'] = $role;
+            $email = $user['email'];
+            $password = $user['password'];
+            $role = $user['role'];
+            $id = $user['id'];
+            $count = $query->rowCount();
+            if ($count == 1) {
 
-                    if ($role == 'ADMIN') {
-                        header("location:listeutilisateur.php");
-                    } else
-                        header("location:my-profile.php?username=$username&id=$id&email=$email&password=$password");
-                } else {
-                    $error = "Your Login Name or Password is invalid";
-                }
+                $_SESSION['username'] = $username;
+                $_SESSION['password'] = $password;
+                $_SESSION['role'] = $role;
+
+                if ($role == 'ADMIN') {
+                    header("location:listeutilisateur.php");
+                } else
+                    header("location:my-profile.php?username=$username&id=$id&email=$email&password=$password");
             } else {
                 $error = "Your Login Name or Password is invalid";
             }
